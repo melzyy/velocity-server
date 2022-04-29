e517c6bf1df1fe279a3af38264b6cdfcc8a62e71
Denis <71926357+melzyy@users.noreply.github.com>
2022-04-29 19:31:11 +0300

    Add files via upload

---
+++ world/start.sh
@@ -0,0 +1,4 @@
+# Screen and AutoPlugs' self-updater don't work very well together.
+# To be concrete, screen closes the terminal when AutoPlug tries to restart with the new version, that's why we do the below.
+# To access the terminal use 'screen -r AUTOPLUG'.
+/usr/bin/screen -DmS minecraft-world sh -c 'java -jar AutoPlug-Client.jar'
